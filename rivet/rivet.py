2a6c212c25f647a4bb0d639bf4576ff5a8ab163d
Paul Philipp Gadow <paul.philipp.gadow@cern.ch>
2023-07-17 16:20:27 +0200

    updates to run scripts of rivet

--- rivet/rivet.py
+++ rivet/rivet.py
@@ -8,7 +8,7 @@ from Rivet_i.Rivet_iConf import Rivet_i
 rivet = Rivet_i()
 import os
 from os.path import join
-rivet.AnalysisPath = join(os.environ['PWD'], 'rivet')
+rivet.AnalysisPath = join(os.environ['TestArea'], '..', 'rivet')
 
 rivet.Analyses += [ 'tttt_parton', 'tttt_event' , 'tttt_ttH']
 rivet.RunName = ''
--- run_rivet.sh
+++ run_rivet.sh
@@ -1,3 +1,43 @@
 #!/bin/bash
 
-athena rivet/rivet.py --filesInput output/100300_13600GeV_281306/test_DSID_100300.EVNT.root
+
+# set DSID
+DSID=${1}
+if [[ -z ${DSID} ]]; then
+    echo "DSID not provided, using 100000 as default.";
+    DSID=100000;
+fi
+
+# center of mass energy (in GeV)
+COMENERGY=${2}
+if [[ -z ${COMENERGY} ]]; then
+    echo "center of mass energy not provided, using 13000. as default.";
+    COMENERGY=13000.;
+fi
+
+# random number generator seed
+SEED=${3}
+if [[ -z ${SEED} ]]; then
+    echo "random seed not provided, using 1234 as default.";
+    SEED=1234;
+fi
+
+# launch job
+TAG=${DSID}_${COMENERGY/.*}GeV_${SEED}
+RESULTDIR=$PWD/output/$TAG
+TMPWORKDIR=/tmp/rivet_$TAG
+RIVETDIR=$PWD/rivet/
+
+rm -rf $TMPWORKDIR && mkdir -p $TMPWORKDIR
+cd $TMPWORKDIR
+athena $RIVETDIR/rivet.py --filesInput $RESULTDIR/test_DSID_${DSID}.EVNT.root
+
+ls
+pwd
+cp $TMPWORKDIR/Rivet_tttt.yoda.gz $RESULTDIR/
+rm -rf $TMPWORKDIR
+cd -
+
+
+
+

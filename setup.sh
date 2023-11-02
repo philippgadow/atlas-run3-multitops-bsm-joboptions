c825c8dc795f907b6b34e17d050d64b5fb65c371
Paul Philipp Gadow <paul.philipp.gadow@cern.ch>
2023-11-02 22:09:21 +0100

    update AthGeneration release, MG3v5

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 source /cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase/user/atlasLocalSetup.sh -q
 
 # r22 default release
-MYRELEASE="AthGeneration,23.6.11"
+MYRELEASE="AthGeneration,23.6.18"
 
 mkdir -p workdir
 cd workdir
